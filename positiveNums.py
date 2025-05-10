8ace3292163e367ad42bf4e4a8d5a4e8ec70cbf3
Nihar <131286660+NiharChuri@users.noreply.github.com>
2025-05-10 18:11:36 +0530

    Create positiveNums.py

---
+++ positiveNums.py
@@ -0,0 +1,10 @@
+list1 = [12, -7, 5, 64, -14]
+
+# Output positive numbers
+print("Input: list1 =", list1)
+print("Output:", end=" ")
+for num in list1:
+    if num > 0:
+        print(num, end=", ")
+
+print("\n")
